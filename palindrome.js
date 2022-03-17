b43c1b4925594e77aa6359ad014fb6bfd3fad751
Ian Lockhart <lockhaim@gmail.com>
2022-03-17 11:35:49 -0600

    commenting done to explain my throught process

--- palindrome.js
+++ palindrome.js
@@ -1,9 +1,9 @@
 // Write a function that takes a string and checks if it is a palindrome.
-function palindrome(str) {
-  let revStr = str.split('').reverse().join('')
-  console.log(revStr)
-  return revStr.toLowerCase() === str.toLowerCase()
+function palindrome(str) { //function takes a string
+  let revStr = str.split('').reverse().join('') //set a variable that takes the string and splits it into an array between each character. It then reverses this array, and joins each element back together.
+  console.log(revStr) //consolelog the reversed string so we can see it
+  return revStr.toLowerCase() === str.toLowerCase() //will return true if the string is a palindrome regardless of case (setting both variables str and revStr to lowercase. If we did not include this it would give strings with an uppercase a value of false because the numerical value held by the same letter with different casing is different.)
 }
 
-console.log(palindrome("Ian"))
-console.log(palindrome("Noon"))
+console.log(palindrome("Ian")) //testing non palindrome
+console.log(palindrome("Noon")) //testing palindrome with uppercase
