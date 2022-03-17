e599cec9ff210ff91da76313bc0375c62965b4c0
Ian Lockhart <lockhaim@gmail.com>
2022-03-17 11:21:59 -0600

    palindrome tested and solved!

--- palindrome.js
+++ palindrome.js
@@ -1,1 +1,9 @@
 // Write a function that takes a string and checks if it is a palindrome.
+function palindrome(str) {
+  let revStr = str.split('').reverse().join('')
+  console.log(revStr)
+  return revStr.toLowerCase() === str.toLowerCase()
+}
+
+console.log(palindrome("Ian"))
+console.log(palindrome("Noon"))
